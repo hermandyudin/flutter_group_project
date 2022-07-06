7d91f9035e4fbfd18db96f7b4e02e2243a110498
hermandyudin <dyudin.german@mail.ru>
2022-07-07 00:49:16 +0300

    [CHANGED] Favourite page UI

--- lib/favorite_page.dart
+++ lib/favorite_page.dart
@@ -24,52 +24,48 @@ class FavoriteNew extends State<FavoriteNewStateful> {
     return Padding(
         padding:
             const EdgeInsets.only(left: 10, top: 15, bottom: 15, right: 15),
-        child: Column(children: <Widget>[
-          GestureDetector(
-              onTap: () => Navigator.push(
+        child: Row(children: <Widget>[
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(180, 800),
+                  maximumSize: const Size(180, 800),
+                  primary: CustomColors.green),
+              onPressed: () => Navigator.push(
                   context,
                   MaterialPageRoute(
                       builder: (context) => FavoriteArtistsStateful())),
-              child: Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                  ),
-                  color: CustomColors.green,
-                  elevation: 10,
-                  child: Padding(
-                      padding: const EdgeInsets.only(
-                          left: 10, top: 15, bottom: 15, right: 15),
-                      child: Row(children: const [
-                        Padding(
-                            padding: EdgeInsets.only(right: 15),
-                            child: Icon(Icons.person)),
-                        Flexible(
-                          child: Text("FAV Artists",
-                              style: TextStyle(fontSize: 17)),
-                        )
-                      ])))),
-          GestureDetector(
-              onTap: () => Navigator.push(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Icon(
+                      Icons.person,
+                      color: Colors.black,
+                      size: 40,
+                    ),
+                    Text("FAV Artists",
+                        style: TextStyle(fontSize: 17, color: Colors.black)),
+                  ])),
+          Spacer(),
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(180, 800),
+                  maximumSize: const Size(180, 800),
+                  primary: CustomColors.green),
+              onPressed: () => Navigator.push(
                   context,
                   MaterialPageRoute(
                       builder: (context) => FavoriteSongsStateful())),
-              child: Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                  ),
-                  color: CustomColors.green,
-                  elevation: 10,
-                  child: Padding(
-                      padding: const EdgeInsets.only(
-                          left: 10, top: 15, bottom: 15, right: 15),
-                      child: Row(children: const [
-                        Padding(
-                            padding: EdgeInsets.only(right: 15),
-                            child: Icon(Icons.music_note)),
-                        Flexible(
-                            child: Text("FAV Songs",
-                                style: TextStyle(fontSize: 17))),
-                      ])))),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Icon(
+                      Icons.music_note,
+                      color: Colors.black,
+                      size: 40,
+                    ),
+                    Text("FAV Songs",
+                        style: TextStyle(fontSize: 17, color: Colors.black)),
+                  ])),
         ]));
   }
 }
