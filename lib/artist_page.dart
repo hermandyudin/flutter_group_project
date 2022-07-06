bf95fb3b479e152808f49afae562f0d7b8242e10
hugowea <z.azizov@innopolis.university>
2022-07-06 20:43:37 +0300

    add search Artist and Favorite pages, add Button in Favourite, ignore check connections

--- lib/artist_page.dart
+++ lib/artist_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter_group_project/parsing.dart';
 import 'package:flutter_group_project/song_page.dart';
 import 'package:flutter_group_project/theme/colors.dart';
 
-
 import 'classes/artist.dart';
 import 'check_connection_methods.dart';
 import 'main.dart';
@@ -16,7 +15,7 @@ class ArtistStateful extends StatefulWidget {
 }
 
 class ArtistPage extends State<ArtistStateful> {
-  final _suggestions = <Artist>[];
+  List filteredList = [];
 
   @override
   Widget build(BuildContext context) {
@@ -27,60 +26,80 @@ class ArtistPage extends State<ArtistStateful> {
           checkConnection(downloadData, context);
           return Future<void>.delayed(const Duration(seconds: 1));
         },
-        child: ListView.builder(
-            padding: const EdgeInsets.all(8),
-            itemCount: artists.length,
-            itemBuilder: (BuildContext context, int index) {
-              final alreadySaved = saved.contains(artists[index]);
-              return GestureDetector(
-                  onTap: () => Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) =>
-                              SongStateful(id: artists[index].id))),
-                  child: Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15.0),
-                      ),
-                      color: CustomColors.green,
-                      elevation: 10,
-                      child: Padding(
-                          padding: const EdgeInsets.only(
-                              left: 10, top: 15, bottom: 15, right: 15),
-                          child: Row(children: [
-                            Padding(
-                                padding: const EdgeInsets.only(right: 15),
-                                child: IconButton(
-                                    icon: Icon(
-                                      alreadySaved
-                                          ? Icons.favorite
-                                          : Icons.favorite_border,
-                                      color: alreadySaved ? Colors.red : null,
-                                      semanticLabel: alreadySaved
-                                          ? 'Remove from saved'
-                                          : 'Save',
-                                    ),
-                                    onPressed: () {
-                                      setState(() {
-                                        if (alreadySaved) {
-                                          saved.remove(artists[index]);
-                                        } else {
-                                          saved.add(artists[index]);
-                                        }
-                                      });
-                                    })),
-                            Padding(
-                                padding: const EdgeInsets.only(right: 15),
-                                child: CircleAvatar(
-                                  radius: 20, // Image radius
-                                  backgroundImage:
-                                      NetworkImage(artists[index].imageUrl),
-                                )),
-                            Flexible(
-                                child: Text(artists[index].name,
-                                    style: const TextStyle(fontSize: 17))),
-                          ]))));
-            }));
+        child: Column(children: <Widget>[
+          Padding(
+              padding: EdgeInsets.all(20),
+              child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Search ',
+                    hintStyle: TextStyle(
+                      fontSize: 14,
+                    ),
+                  ),
+                  onChanged: (text) {
+                    text = text.toLowerCase();
+                    filter(text);
+                  })),
+          Expanded(
+              child: ListView.builder(
+                  padding: const EdgeInsets.all(8),
+                  itemCount: filteredList.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    final alreadySaved = saved.contains(filteredList[index]);
+                    return GestureDetector(
+                        onTap: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    SongStateful(id: filteredList[index].id))),
+                        child: Card(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15.0),
+                            ),
+                            color: CustomColors.green,
+                            elevation: 10,
+                            child: Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 10, top: 15, bottom: 15, right: 15),
+                                child: Row(children: [
+                                  Padding(
+                                      padding: const EdgeInsets.only(right: 15),
+                                      child: IconButton(
+                                          icon: Icon(
+                                            alreadySaved
+                                                ? Icons.favorite
+                                                : Icons.favorite_border,
+                                            color: alreadySaved
+                                                ? Colors.red
+                                                : null,
+                                            semanticLabel: alreadySaved
+                                                ? 'Remove from saved'
+                                                : 'Save',
+                                          ),
+                                          onPressed: () {
+                                            setState(() {
+                                              if (alreadySaved) {
+                                                saved.remove(
+                                                    filteredList[index]);
+                                              } else {
+                                                saved.add(filteredList[index]);
+                                              }
+                                            });
+                                          })),
+                                  Padding(
+                                      padding: const EdgeInsets.only(right: 15),
+                                      child: CircleAvatar(
+                                        radius: 20, // Image radius
+                                        backgroundImage: NetworkImage(
+                                            filteredList[index].imageUrl),
+                                      )),
+                                  Flexible(
+                                      child: Text(filteredList[index].name,
+                                          style:
+                                              const TextStyle(fontSize: 17))),
+                                ]))));
+                  }))
+        ]));
   }
 
   @override
@@ -97,5 +116,13 @@ class ArtistPage extends State<ArtistStateful> {
             artists.add(value);
           }));
     }
+    filteredList = artists;
+  }
+
+  void filter(String inputString) {
+    filteredList = artists
+        .where((i) => i.name.toLowerCase().contains(inputString))
+        .toList();
+    setState(() {});
   }
 }
